e102fc378f24dd36655f087547bdb49ba6eba97b
RK Davies <rkdavies@RKs-MacBook-Pro.local>
2022-07-29 04:21:54 -0700

    S saves a video -30s, x stops recording, s takes an image, p pauses, anykey unpauses... maybe P should unpause so we can s

--- MoDe.py
+++ MoDe.py
@@ -5,10 +5,91 @@
 # Sensitivity is VERY HIGH. increase 15,15 to something higher (25,25) if needed
 ##### 
 import cv2
+import datetime
 import os
 import sys
 import pafy
 import time
+from collections import deque
+from threading import Thread
+from queue import Queue
+
+buffer_size = 684
+# Shameless jack of code 
+class KeyClipWriter:
+    def __init__(self, bufSize=buffer_size, timeout=1.0):
+        # store the maximum buffer size of frames to be kept
+        # in memory along with the sleep timeout during threading
+        self.bufSize = bufSize
+        self.timeout = timeout
+        # initialize the buffer of frames, queue of frames that
+        # need to be written to file, video writer, writer thread,
+        # and boolean indicating whether recording has started or not
+        self.frames = deque(maxlen=bufSize)
+        self.Q = None
+        self.writer = None
+        self.thread = None
+        self.recording = False
+
+    def update(self, frame):
+        # update the frames buffer
+        self.frames.appendleft(frame)
+        # if we are recording, update the queue as well
+        if self.recording:
+            self.Q.put(frame)
+
+    def start(self, outputPath, fourcc, fps):
+        # indicate that we are recording, start the video writer,
+        # and initialize the queue of frames that need to be written
+        # to the video file
+        self.recording = True
+        self.writer = cv2.VideoWriter(outputPath, fourcc, fps,
+             (1920,1080), True)
+        #    (self.frames[0].shape[1], self.frames[0].shape[0]), True)
+        self.Q = Queue()
+        # loop over the frames in the deque structure and add them
+        # to the queue
+        for i in range(len(self.frames), 0, -1):
+            self.Q.put(self.frames[i - 1])
+        # start a thread write frames to the video file
+        self.thread = Thread(target=self.write, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+    def write(self):
+        # keep looping
+        while True:
+            # if we are done recording, exit the thread
+            if not self.recording:
+                return
+            # check to see if there are entries in the queue
+            if not self.Q.empty():
+                # grab the next frame in the queue and write it
+                # to the video file
+                frame = self.Q.get()
+                self.writer.write(frame)
+            # otherwise, the queue is empty, so sleep for a bit
+            # so we don't waste CPU cycles
+            else:
+                time.sleep(self.timeout)
+
+    def flush(self):
+        # empty the queue by flushing all remaining frames to file
+        while not self.Q.empty():
+            frame = self.Q.get()
+            self.writer.write(frame)
+
+    def finish(self):
+        # indicate that we are done recording, join the thread,
+        # flush all remaining frames in the queue to file, and
+        # release the writer pointer
+        self.recording = False
+        self.thread.join()
+        self.flush()
+        self.writer.release()
+
+kcw = KeyClipWriter(bufSize=buffer_size)
+consecFrames = 0
 
 out_dir = './stills'
 
@@ -36,6 +117,8 @@ if 'http' in sys.argv[1]:
     url = sys.argv[1]
     video = pafy.new(url)
     v_title = video.title
+    v_streams = video.allstreams
+    v_stream = v_streams[4]
     best = video.getbest(preftype="mp4")
 
     baseline_image=None
@@ -52,6 +135,7 @@ while True:
     status=0
     gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray_frame=cv2.GaussianBlur(gray_frame,(gnum,gnum),0)
+    updateConsecFrames = True
 
     if baseline_image is None:
         baseline_image=gray_frame
@@ -71,11 +155,11 @@ while True:
 
     if show_status==1:
         # Using cv2.putText() method
-        frame = cv2.putText(frame, "( g|G )aussianBlur:" + str(gnum), g_org, font,
+        frame = cv2.putText(frame, "gGaussianBlur:" + str(gnum), g_org, font,
                            fontScale, color, thickness, cv2.LINE_AA)
-        frame = cv2.putText(frame, "(<c|C>)ontourArea:" + str(cnum), c_org, font,
+        frame = cv2.putText(frame, "<cC>ontourArea:" + str(cnum), c_org, font,
                            fontScale, color, thickness, cv2.LINE_AA)
-        frame = cv2.putText(frame, "( d|D )elta:" + str(dnum), d_org, font,
+        frame = cv2.putText(frame, "dDelta:" + str(dnum), d_org, font,
                            fontScale, color, thickness, cv2.LINE_AA)
 
     #cv2.imshow("gray_frame Frame",gray_frame)
@@ -137,12 +221,32 @@ while True:
             cnum = 10000 
             dnum = 5
     if key == ord('s'):
-        img_name = "opencv_frame_{}.png".format(time.time())
+        timestamp = datetime.datetime.now()
+        img_name = "{}/{}.png".format(out_dir, 
+                   timestamp.strftime("%Y%m%d-%H%M%S"))
         cv2.imwrite(img_name, frame)
         count += 1
+    if key == ord('S'):
+        if not kcw.recording:
+            timestamp = datetime.datetime.now()
+            p = "{}/{}.avi".format(out_dir,
+                timestamp.strftime("%Y%m%d-%H%M%S"))
+            kcw.start(p, cv2.VideoWriter_fourcc(*"X264"),20)
+    if updateConsecFrames:
+        consecFrames += 1
+    # update the key frame clip buffer
+    kcw.update(frame)
+    # if we are recording and reached a threshold on consecutive
+    # number of frames with no action, stop recording the clip
+    if kcw.recording and consecFrames == buffer_size:
+        kcw.finish()
+    if key == ord('x'):
+        kcw.finish()
     if key == ord('p'):
         cv2.waitKey(-1)
 
+if kcw.recording:
+    kcw.finish()
 #Clean up, Free memory
 video.release()
 cv2.destroyAllWindows
